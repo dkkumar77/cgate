0c259d99ced97ef7ec36e316b7ee93b14543a2a5
Deepak Kumar <bcs.deepakkumar@gmail.com>
2024-05-25 03:57:58 -0400

    setup

---
+++ driver.c
@@ -0,0 +1,9 @@
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define http_port 80
+#define https_port 443
+#define PORT 8080
